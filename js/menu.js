54dbfe33b5da79c02887c7484103e9cb916a8e05
Thiagowski <thiagopereira3@hotmail.com>
2024-11-06 15:19:14 -0300

    adicionando o javascript

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+// Selecionando a palavra "Menu"
+const botaoMenu = document.querySelector(".titulo-menu a");
+
+// Selecionando a lista de links do menu
+const listaDeLinks = document.querySelector(".links-menu");
+
+// Ouvinte de evento para clicar no botão menu
+botaoMenu.addEventListener("click", function (event) {
+    // Anulando o comportamento padrão do link
+    event.preventDefault();
+
+    //    Alternar a classe aberto entre ativado/desativado
+    listaDeLinks.classList.toggle("aberto");
+
+    if (listaDeLinks.classList.contains("aberto")) {
+        botaoMenu.innerHTML = "Fechar &times;"
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;"
+    }
+
+});
