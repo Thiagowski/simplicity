238b02dbe14480d44b71d7d4d8930b0fdee5507a
Thiagowski <thiagopereira3@hotmail.com>
2024-11-06 15:32:32 -0300

    contatos

--- js/contato.js
+++ js/contato.js
@@ -8,3 +8,15 @@ const campoCidade = document.querySelector("#cidade");
 const campoEstado = document.querySelector("#estado");
 const botaoBuscar = document.querySelector("#buscar");
 const mensagemStatus = document.querySelector("#status");
+
+// ouvinte de evento para o botão Buscar
+botaoBuscar.addEventListener("click", function(){
+// verificando se o CEP digitado NÃO TEM 9 DIGITOS  
+if(campoCep.value.lenght !== 9){
+
+mensagemStatus.innerHTML = "Digite um CEP válido!";
+mensagemStatus.style.color = "purple";
+return;
+
+}
+});
